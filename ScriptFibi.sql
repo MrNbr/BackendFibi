9ac4b100b23725306838a81f4a89a010dbc1df3a
KoalaGod <rubenebur7@gmail.com>
2019-10-12 17:58:54 +0200

    Added SQL script used to generate tables

---
+++ ScriptFibi.sql
@@ -0,0 +1,25 @@
+CREATE TABLE "assignment" (
+	assignmentname varchar(30) primary key
+);
+
+INSERT INTO "assignment" values ('DBD');
+INSERT INTO "assignment" values ('PES');
+INSERT INTO "assignment" values ('ASW');
+INSERT INTO "assignment" values ('ER');
+INSERT INTO "assignment" values ('GPS');
+INSERT INTO "assignment" values ('PROP');
+INSERT INTO "assignment" values ('FM');
+INSERT INTO "assignment" values ('M1');
+INSERT INTO "assignment" values ('M2');
+INSERT INTO "assignment" values ('PRO1');
+
+CREATE TABLE "aula" (
+	aula varchar(6) primary key
+);
+
+INSERT INTO "aula" values('A1-102');
+INSERT INTO "aula" values('A2-E01');
+INSERT INTO "aula" values('B2-202');
+INSERT INTO "aula" values('A6-205');
+INSERT INTO "aula" values('A2-E02');
+INSERT INTO "aula" values('A5-002');
